2202f06357ba464936ed6f3fe2ceb31e7021c1bd
Terra Brown <superloach42@gmail.com>
2022-08-25 23:47:10 -0400

    move args and env to pre

--- example/main.go
+++ example/main.go
@@ -20,7 +20,6 @@ import (
 	"flag"
 	"fmt"
 	"os"
-	"strings"
 	"syscall/js"
 )
 
@@ -29,10 +28,11 @@ func main() {
 	fmt.Println(flag.Args())
 
 	p := js.Global().Get("document").Call("createElement", "p")
-	p.Set("innerText", strings.Join([]string{
-		"Hello, World!",
-		fmt.Sprintf("args=%q", os.Args),
-		fmt.Sprintf("env=%q", os.Environ()),
-	}, "\n"))
+	p.Set("innerText", "Hello, World!")
 	js.Global().Get("document").Get("body").Call("appendChild", p)
+
+	pre := js.Global().Get("document").Call("createElement", "pre")
+	pre.Set("innerText", fmt.Sprintf("args: %q\nevn: %q", os.Args, os.Environ()))
+	pre.Get("style").Set("whiteSpace", "pre-wrap")
+	js.Global().Get("document").Get("body").Call("appendChild", pre)
 }
