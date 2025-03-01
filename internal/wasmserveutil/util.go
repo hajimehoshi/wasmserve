8f351536b29aef9211be5f086902e962c9c6982f
Hajime Hoshi <hajimehoshi@gmail.com>
2025-03-01 21:26:28 +0900

    internal/wasmserve: fix error messages

--- internal/wasmserveutil/util.go
+++ internal/wasmserveutil/util.go
@@ -15,7 +15,7 @@ var reGoVersion = regexp.MustCompile(`^go(\d+)\.(\d+)(\.(\d+))?`)
 func WasmExecJSURL(goVersion string) (string, error) {
 	m := reGoVersion.FindStringSubmatch(goVersion)
 	if len(m) == 0 {
-		return "", fmt.Errorf("wasmserve: invalid Go version: %s", goVersion)
+		return "", fmt.Errorf("wasmserveutil: invalid Go version: %s", goVersion)
 	}
 	minor, _ := strconv.Atoi(m[2])
 
